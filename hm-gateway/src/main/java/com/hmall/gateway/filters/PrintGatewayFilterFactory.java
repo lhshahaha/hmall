f04298f594d4f45ae9790de4de86b92a13a8081f
husruo <2392541095@qq.com>
2024-10-25 09:01:36 +0800

    gateway

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/PrintGatewayFilterFactory.java
@@ -0,0 +1,21 @@
+package com.hmall.gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PrintGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+    @Override
+    public GatewayFilter apply(Object config) {
+        return new GatewayFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                return chain.filter(exchange);
+            }
+        };
+    }
+}
